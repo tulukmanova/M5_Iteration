a553c87f6c7d8545826c95903cfd7a42c9126cb0
tulukmanova <tulukmanova@my.nl.edu>
2024-10-29 14:36:51 -0500

    Add files via upload

---
+++ b.py
@@ -0,0 +1,10 @@
+#Takhmina
+#10/22/24
+numbers = [87,77,6,20,10,53,15,12,5,45]
+
+for number in numbers:
+    if number %2 == 0:
+        print(f"Hey - {number} is even!")
+    else:
+        print(f"Gosh, {number} is odd!")
+            
